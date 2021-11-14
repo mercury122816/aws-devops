27a1aca6c046dde14f1671e703445b5ad90a5bc8
Samriddha Chatterjee <samriddhac@gmail.com>
2021-11-14 22:13:05 +0530

    update

--- code/ssm/managed-instance-setup.sh
+++ code/ssm/managed-instance-setup.sh
@@ -4,5 +4,5 @@ curl https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/
 sudo yum install -y /tmp/ssm/amazon-ssm-agent.rpm
 sudo systemctl stop amazon-ssm-agent
 # edit the code, id and region in the command below
-sudo amazon-ssm-agent -register -code "activation-code" -id "activation-id" -region "region"
+sudo amazon-ssm-agent -register -code "o/bQ/0KMiUvqPdzMxH55" -id "cdaa41bf-7df0-4904-b94b-3aabfa7e45ed" -region "us-east-1"
 sudo systemctl start amazon-ssm-agent
